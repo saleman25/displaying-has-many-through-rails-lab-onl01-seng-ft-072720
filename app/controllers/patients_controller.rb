5c87e74182267b92cef5b121a54b8e8eb631b01e
Samantha Aleman <samanthaaleman@samanthas-air.home>
2020-09-30 15:53:06 -0700

    Done.

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,21 @@
+class PatientsController < ApplicationController
+    before_action :set_patient, only: [:show]
+
+    def index
+        @patient = Patient.all 
+    end 
+
+    def show
+    end 
+
+    private 
+
+    def set_patient 
+        @patient = Patient.find(params[:id])
+    end 
+
+    def patient_params
+        params.require(:patient).permit(:name, :age)
+    end 
+
+end 
